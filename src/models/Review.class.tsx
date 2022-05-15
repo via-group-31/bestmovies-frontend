89c1527a82762d5fcd125bf469a576fe0f00901f
popalnicolas <popal.nicolas@gmail.com>
2022-05-15 21:24:33 +0200

    Models setters getters

--- src/models/Review.class.tsx
+++ src/models/Review.class.tsx
@@ -1,13 +1,37 @@
 import Movie from "./Movie.class";
 
 export default class Review{
-    private reviewContent: string;
-    private userID: number;
-    private movieID: number;
+    private _reviewContent: string;
+    private _userID: number;
+    private _movieID: number;
 
     constructor(reviewContent: string, userID: number, movieID: number){
-        this.reviewContent = reviewContent;
-        this.userID = userID;
-        this.movieID = movieID;
+        this._reviewContent = reviewContent;
+        this._userID = userID;
+        this._movieID = movieID;
+    }
+
+    public get reviewContent(): string{
+        return this._reviewContent;
+    }
+
+    public get userID(): number{
+        return this._userID;
+    }
+
+    public get movieID(): number{
+        return this._movieID;
+    }
+
+    public set reviewContent(reviewContent: string){
+        this._reviewContent = reviewContent;
+    }
+
+    public set userID(userID: number){
+        this._userID = userID;
+    }
+
+    public set movieID(movieID: number){
+        this._movieID = movieID;
     }
 }
--- src/models/Users.class.tsx
+++ src/models/Users.class.tsx
@@ -1,27 +1,41 @@
 export default class Users{
-    private _userID?: number;
-    private _username?: string;
-    private _email?: string;
+    private _userID: number;
+    private _username: string;
+    private _email: string;
     private _accessToken: string;
 
-    constructor(accessToken: string){
+    constructor(userID: number, username: string, email: string, accessToken: string){
+        this._userID = userID;
+        this._username = username;
+        this._email = email;
         this._accessToken = accessToken;
     }
 
-    /* GETTERS */
-    public get UserID(): number | undefined{
+    public get UserID(): number{
         return this._userID;
     }
 
-    public get username(): string | undefined{
+    public get username(): string{
         return this._username;
     }
 
-    public get email(): string | undefined{
+    public get email(): string{
         return this._email;
     }
 
     public get accessToken(): string{
         return this._accessToken;
+    }
+
+    public set userID(userID: number){
+        this._userID = userID;
+    }
+
+    public set username(username: string){
+        this._username = username;
+    }
+
+    public set email(email: string){
+        this._email = email;
     }
 }
